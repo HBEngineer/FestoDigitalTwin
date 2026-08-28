c4ed49d45ac72aba37b18b7c22262871bdf6fd27
HBEngineer <52140316+HBEngineer@users.noreply.github.com>
2026-08-28 15:38:31 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -95,7 +95,7 @@ const arGroup = new THREE.Group();
 scene.add(arGroup);
 
 // --- GRID HELPER ---
-const gridHelper = new THREE.GridHelper(10, 20, 0x969696, 0x666666);
+const gridHelper = new THREE.GridHelper(10, 20, 0xFFFFFF, 0x666666);
 gridHelper.position.y = -0.01;
 arGroup.add(gridHelper);
 
